6005c250318f419c5a618b9e5eca0f51f03b9fba
sapzzil <bloodatils@hanmail.net>
2024-08-26 08:50:25 +0900

    결과해석

---
+++ research_4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 12:14:16 2024
+
+@author: sapzzil
+
+결과 분석
+"""
+
+import pandas as pd
+
+cls_result = 'research_2_model_result.csv'
+reg_result = 'research_3_model_result.csv'
+
+
+result_2 = pd.read_csv(cls_result)
+result_3 = pd.read_csv(reg_result)
+
