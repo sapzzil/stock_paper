398b218db1aa01115586c5024c1155beda147ba9
sapzzil <bloodtails@hanmail.net>
2024-08-28 18:05:36 +0900

    결과해석

--- research_4.py
+++ research_4.py
@@ -16,3 +16,6 @@ reg_result = 'research_3_model_result.csv'
 result_2 = pd.read_csv(cls_result)
 result_3 = pd.read_csv(reg_result)
 
+for col in result_3.loc[:,result_3.columns.str.contains('R-squared')].columns:
+    print(f'{col}')
+    print(result_3.nlargest(10, col))
